8b5a95ff92ebe30aaa4f6b8c49a818072906d0fd
Anurag Samarth <asamarth017@gmail.com>
2024-01-08 10:15:06 +0530

    Some Chenges had been done in JS file.

--- Day 4 Password Toggle with JavaScript/script.js
+++ Day 4 Password Toggle with JavaScript/script.js
@@ -1,17 +1,28 @@
 const password = document.querySelector(".Password");
-const togglePassword = document.querySelector(".fa-regular");
+const togglePassword = document.querySelector(".eye_toggle");
 const submit = document.querySelector(".submit");
 
 submit.addEventListener("click", (e)=>{
           e.preventDefault();
 })
 
-togglePassword.addEventListener("click", ()=>{
+// togglePassword.addEventListener("click", ()=>{
 
-      const type = togglePassword.getAttribute("type") === "password" ? "text": "password";
+//       const type = togglePassword.getAttribute("type") === "password" ? "text": "password";
       
-      password.setAttribute("type", type);
-    //   console.log(password);
+//       password.setAttribute("type", type);
+//     //   console.log(password);
     
-    this.classList.toggle('fa-regular');
+//     this.classList.toggle('fa-regular');
+// })
+
+
+togglePassword.addEventListener('click', ()=>{
+     if(password.type === "password"){
+        password.type = "text";
+        // togglePassword.style.display = "display";
+     }else{
+      password.type = "password";
+      // togglePassword.style.display = "none";
+     }
 })
