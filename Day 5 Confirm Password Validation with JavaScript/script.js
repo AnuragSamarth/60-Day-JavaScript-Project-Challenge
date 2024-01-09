8289b7fd7c0fec55f27f8a8db34c33f968d1439a
Anurag Samarth <asamarth017@gmail.com>
2024-01-09 12:54:13 +0530

    Day-5(with some changes)

--- Day 5 Confirm Password Validation with JavaScript/script.js
+++ Day 5 Confirm Password Validation with JavaScript/script.js
@@ -4,11 +4,15 @@ const confirmPassword  = document.querySelector("#confirmPassword");
 const submitBtn = document.querySelector(".submitBtn");
 const passwordForm = document.querySelector(".passwordForm");
 const messageText1 = document.querySelector(".messageText1");
+const name = document.querySelector(".inputField");
+const showPassword = document.querySelector(".showPassword");
 
 submitBtn.addEventListener("click", (e) =>{
     e.preventDefault();
     const passwordValue = enterPassword.value;
     const passwordConfirm = confirmPassword.value;
+    const nameValue = name.value;
+    if(passwordValue !== "" && passwordConfirm !== "" && nameValue !== "" ){
     if(passwordValue != passwordConfirm){
         setTimeout(()=>{
             messageText.innerHTML = "Password is incorrect"
@@ -21,15 +25,31 @@ submitBtn.addEventListener("click", (e) =>{
               },3000)
         })
     }else{
-        
             messageText.innerHTML = "Password is correct"
               messageText.classList.add('messageText1');
+              messageText.style.backgroundColor = "green";
             setTimeout(() => {
                 messageText.innerHTML = ""
                 messageText.classList.remove('messageText1');
             }, 3000);
-
-    }
-    // console.log(passwordConfirm);
-    // console.log(passwordValue);
+    } 
+} else{
+    messageText.innerHTML = "Plz enter the below field"
+              messageText.classList.add('messageText1');
+              messageText.style.backgroundColor = "yellow";
+            setTimeout(() => {
+                messageText.innerHTML = ""
+                messageText.classList.remove('messageText1');
+            }, 3000);
+}
 });
+
+
+// Create showing password field
+showPassword.addEventListener('click',()=>{
+   if(enterPassword.type === 'password'){
+    enterPassword.type = 'text';
+   }else{
+    enterPassword.type = 'password';
+   }
+})
