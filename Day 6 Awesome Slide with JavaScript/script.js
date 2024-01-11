bcfc5ae8308bfa4ce15cd30654e138120fe86194
Anurag Samarth <asamarth017@gmail.com>
2024-01-11 06:40:47 +0530

    Add color changer effect

--- Day 6 Awesome Slide with JavaScript/script.js
+++ Day 6 Awesome Slide with JavaScript/script.js
@@ -1,19 +1,45 @@
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
+const imgContainer = document.querySelector(".image-container");
 
 let flag = 0;
 
 function controller(x){
-    flag  += flag; 
+    flag  = flag + 1; 
     slideshow(flag);
 }
 
 function slideshow(num){
     let slides = document.getElementsByClassName("image-slider");
+
+    if(num === slides.length){
+        flag = 0;
+        num = 0;
+    }
+    if(num < 0){
+        flag = slides.length - 1;
+        num = slides.length - 1;
+    }
     
     for(let y of slides){
      y.style.display = 'none';
     }
 
     slides[num].style.display = "block";
+}
+
+
+// color changer effect.
+prev.addEventListener("click", ()=>{
+     imgContainer.style.backgroundColor = colorChanger();
+})
+next.addEventListener("click", ()=>{
+    imgContainer.style.backgroundColor = colorChanger();
+});
+
+function colorChanger(){
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    return `rgb(${red} ${green} ${blue})`;
 }
