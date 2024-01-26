23a7187aa067ff4eb2239dbd6fcb4c7d0b5fa270
Anurag Samarth <asamarth017@gmail.com>
2024-01-26 21:02:37 +0530

    Progress Bar with GSAP

---
+++ Day 10 Animated Progress Bar with JavaScript and GSAP/script.js
@@ -0,0 +1,30 @@
+gsap.to(".html-bar", {
+    width: "90%",
+    backgroundColor: 'rgba(208, 81, 52, 0.793)',
+    duration: 2, 
+    ease: "bounce.out" 
+})
+gsap.to(".css-bar", {
+    width: "80%",
+    backgroundColor: 'rgba(208, 81, 52, 0.793)',
+    duration: 2, 
+    ease: "bounce.out" 
+})
+gsap.to(".JS-bar", {
+    width: "70%",
+    backgroundColor: 'rgba(208, 81, 52, 0.793)',
+    duration: 2, 
+    ease: "bounce.out" 
+})
+gsap.to(".ReactJS-bar", {
+    width: "20%",
+    backgroundColor: 'rgba(208, 81, 52, 0.793)',
+    duration: 2, 
+    ease: "bounce.out" 
+})
+gsap.to(".gsap-bar", {
+    width: "10%",
+    backgroundColor: 'rgba(208, 81, 52, 0.793)',
+    duration: 2, 
+    ease: "bounce.out" 
+})
