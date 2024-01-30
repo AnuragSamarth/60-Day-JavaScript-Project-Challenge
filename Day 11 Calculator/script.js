2e79a57ff983d4335a23f67096b0be7ff551c529
Anurag Samarth <asamarth017@gmail.com>
2024-01-31 00:47:51 +0530

    Calculator

---
+++ Day 11 Calculator/script.js
@@ -0,0 +1,21 @@
+let input = document.querySelector("#input");
+let button = document.querySelectorAll("button");
+let output = document.querySelector(".output");
+
+button.forEach((element)=>{
+      element.addEventListener("click", (e)=>{
+       console.log(e.target.textContent);
+
+       if(e.target.textContent === "C"){
+        input.innerText = "";
+        output.innerText = "";
+       }else if(e.target.textContent === "<"){
+         input.innerText = input.innerText.slice(0 , -1);
+       }else if(e.target.textContent === "="){
+        output.innerText = eval(input.innerText);
+       }
+       else{
+        input.innerText += e.target.textContent;
+       }
+      })
+})
