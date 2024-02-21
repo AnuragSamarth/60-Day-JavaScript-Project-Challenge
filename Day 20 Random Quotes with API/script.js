117a3241dd320d8d40e977371175e11648796cb5
Anurag Samarth <asamarth017@gmail.com>
2024-02-21 14:15:35 +0530

    Random Quotes feature are build

--- Day 20 Random Quotes with API/script.js
+++ Day 20 Random Quotes with API/script.js
@@ -1,18 +1,29 @@
 const btn = document.querySelector(".btn");
+const quotesContainer = document.querySelector(".quotes-container");
+const text = document.querySelector(".text");
+const textAuthor = document.querySelector(".text-author");
 
 btn.addEventListener("click", randomQuote);
 
 function randomQuote(){
-    let randomNumber = Math.floor(Math.random() * 100);
     
     const quotes = async()=>{
-       const res = await fetch(`https://api.breakingbadquotes.xyz/v1/quotes/${randomNumber}`);
-
-       const data = res.json();
-
+        try{
+            const res = await fetch(`https://api.quotable.io/quotes/random`);
+            if(!res.ok){
+                throw Error(res.statusText);
+            }
+       console.log(res);
+       const data = await res.json();
        console.log(data);
-    // console.log(res);
+       const content = data[0].content;
+       const author = data[0].author;
+       text.innerText = content;
+       textAuthor.innerText = author;
+       
+        }catch(error){
+            console.log(error);
+        }
     }
-
     quotes();
 }
