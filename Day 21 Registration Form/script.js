974d4c23eb1879bdfee2641a5ce79f455b4a472c
Anurag Samarth <asamarth017@gmail.com>
2024-02-22 00:04:51 +0530

    create varification input field

---
+++ Day 21 Registration Form/script.js
@@ -0,0 +1,22 @@
+const inputField = document.querySelector(".input-field");
+const form = document.querySelector("form");
+const inputFirstName = document.querySelector(".first");
+const inputLastName = document.querySelector(".last");
+const inputEmail = document.querySelector(".email");
+const inputPassword = document.querySelector(".pass");
+const inputRepassword = document.querySelector(".repass");
+
+form.addEventListener("submit", (e)=>{
+      e.preventDefault();
+      if(!inputFirstName.value){
+        alert("Please enter a first name");
+      }else if(!inputLastName.value){
+        alert("Please enter a last name");
+      }else if(!inputEmail.value){
+        alert("Please Enter the mail");
+      }else if(!inputPassword.value){
+        alert("Please Enter the password");
+      }else if(!inputRepassword.value){
+        alert("Please Enter the same password");
+      }
+})
