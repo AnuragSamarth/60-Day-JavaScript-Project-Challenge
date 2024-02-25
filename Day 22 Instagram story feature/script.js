2557ea1dec10d5371698a16e2ad2cd17c427655f
Anurag Samarth <asamarth017@gmail.com>
2024-02-25 19:44:28 +0530

    successfully build instagram story feature

---
+++ Day 22 Instagram story feature/script.js
@@ -0,0 +1,39 @@
+const arr = [
+    {
+        dp:'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        story:'https://images.unsplash.com/photo-1611601679655-7c8bc197f0c6?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        dp: 'https://images.unsplash.com/photo-1524502397800-2eeaad7c3fe5?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        story:'https://images.unsplash.com/photo-1562572159-4efc207f5aff?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        dp:'https://images.unsplash.com/photo-1616529735204-6799853ed36f?q=80&w=1904&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        story:'https://images.unsplash.com/photo-1609748106519-2874fd373d59?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        dp:'https://images.unsplash.com/flagged/photo-1551854716-8b811be39e7e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        story:'https://images.unsplash.com/photo-1600685890506-593fdf55949b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    }
+]
+
+let stories = document.querySelector(".stories");
+let clutter = '';
+arr.forEach((elem,id)=>{
+   clutter += ` <div class="story">
+   <img id="${id}" src="${elem.dp}" alt="">
+</div>`;
+})
+stories.innerHTML = clutter;
+
+stories.addEventListener("click", (val)=>{
+    document.querySelector(".full-story").style.display = "block";
+    document.querySelector(".full-story").style.backgroundImage = `url(${arr[val.target.id].story})`;
+
+    setTimeout(()=>{
+    document.querySelector(".full-story").style.display = "none";
+    },3000);
+})
+
+
+
