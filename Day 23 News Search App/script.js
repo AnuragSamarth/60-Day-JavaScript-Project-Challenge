1da1df97cbbce3c28a955a80b24132bd57abbd2f
Anurag Samarth <asamarth017@gmail.com>
2024-02-27 20:48:20 +0530

    news app done by my side

---
+++ Day 23 News Search App/script.js
@@ -0,0 +1,41 @@
+const apiKey = 'a3213f1583e6456195577527cb5efa2d';
+
+const searchBox = document.querySelector(".search-box");
+const btn = document.querySelector(".btn");
+const newsContainer = document.querySelector(".news-container");
+
+btn.addEventListener("click", ()=>{
+    let input = searchBox.value;
+    // console.log(input);
+    newsFetch(input);
+    searchBox.value = '';
+});
+
+async function newsFetch(input){
+    try{
+     let res = await fetch(`https://newsapi.org/v2/everything?q=${input}&from=2024-01-27&sortBy=publishedAt&apiKey=${apiKey}`);
+
+     if(!res.ok){
+        throw Error("Some thing went wrong. Please try again");
+     }
+     let data = await res.json();
+    //  console.log(data);
+    let articlesArray = data.articles;
+    // console.log(articlesArray);
+
+    let clutter = '';
+    articlesArray.forEach(elem =>{
+       clutter += `<div class="news-box">
+       <div class="news-img">
+           <img src="${elem.urlToImage}" alt="${elem.url}">
+       </div>
+       <h2>${elem.title}</h2>
+       <p>${elem.description}</p>
+   </div>`
+    })
+    newsContainer.innerHTML = clutter;
+    }
+    catch(error){
+        alert(error);
+    }
+}
