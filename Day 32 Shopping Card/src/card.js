e3877099707082b0a890d766927dd0d3df3d61a8
Anurag Samarth <asamarth017@gmail.com>
2024-03-17 12:59:18 +0530

    build card page

--- Day 32 Shopping Card/src/card.js
+++ Day 32 Shopping Card/src/card.js
@@ -1,3 +1,6 @@
+const label = document.querySelector("#label");
+const shoppingCart = document.querySelector("#shopping-cart");
+
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let calculation = ()=>{
@@ -5,3 +8,96 @@ let calculation = ()=>{
     totalItem.innerHTML =  basket.map(elem => elem.item).reduce((x,y) => (x+y),0)
 }
 calculation();
+
+const generateCartItem = ()=>{
+    if(basket.length !== 0){
+       return (shoppingCart.innerHTML = basket.map((x)=>{
+        let {id,item} = x;
+        let search = productArray.find((y) => y.id === id) || [];
+       return `
+          <div class="cart-item">
+             <div class="div-img">
+             <img src=${search.img} alt="img not found" />
+             </div>
+             <div class="details">
+                <div class="title-price-x">
+                  <h4>
+                  <p>${search.name}</p>
+                  </h4>
+                  <p class="cart-item-price">$ ${search.price}</p>
+                  <i class="ri-close-fill" onclick="removeItem(${id})"></i>
+                </div>
+                <div class="add-card">
+                        <button id='dec' onclick="decrement(${id})">-</button>
+                        <span id=${id}>${item}</span>
+                        <button id="inc" onclick="increment(${id})">+</button>
+                </div>
+                <h3>$${item * search.price}</h3>
+             </div>
+          </div>
+        `
+       }).join(""))
+    }else{
+     shoppingCart.innerHTML = ''
+     label.innerHTML = `
+     <h2>Cart is Empty</h2>
+     <a href="index.html">
+      <button class='HomeBtn'>Back to home page</button>
+     </a>
+     `
+    }
+}
+
+generateCartItem();
+
+const increment = (id)=>{
+    let selectedItem = id;
+//   console.log(selectedItem.id)
+   let search = basket.find((x) => x.id === selectedItem.id);
+    if(search === undefined){
+        basket.push({
+            id: selectedItem.id,
+            item: 1
+        })
+    }else{
+        search.item += 1;
+    }
+    // console.log(basket);
+    generateCartItem();
+    update(selectedItem.id);
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+const decrement = (id)=>{
+    let selectedItem = id;
+    // console.log(selectedItem.id)
+    let search = basket.find((x) => x.id === selectedItem.id);
+
+    if(search === undefined) return;
+    else if(search.item === 0) return; 
+    else{
+        search.item -= 1;
+    }
+    update(selectedItem.id)
+    basket = basket.filter((x) => x.item !== 0);
+    generateCartItem();
+    // console.log(basket);
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+const update = (id)=>{
+    //    console.log(id)
+       let search = basket.find((x) => x.id === id);
+    //    console.log(search)
+    document.getElementById(id).innerHTML = search.item;
+    calculation();
+}
+
+const removeItem = (id)=>{
+      let selectedItem = id
+    //   console.log(selectedItem.id)
+    basket = basket.filter((x)=> x.id !== selectedItem.id);
+    localStorage.setItem("data", JSON.stringify(basket));
+    generateCartItem();
+}
+
--- Day 32 Shopping Card/src/data.js
+++ Day 32 Shopping Card/src/data.js
@@ -1,7 +1,7 @@
 const productArray = [
     {
       id:'khjfkds',
-      name:'Unpaired red Nike sneaker',
+      name:'Nike sneaker',
       disc: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
       price: 45,
       img: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
@@ -15,7 +15,7 @@ const productArray = [
     },
     {
       id: 'wkfibisisdf',
-      name: 'Cosmetic Branding Scene',
+      name: 'Cosmetic',
       disc: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
       price: 56,
       img: 'https://images.unsplash.com/photo-1627384113743-6bd5a479fffd?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
