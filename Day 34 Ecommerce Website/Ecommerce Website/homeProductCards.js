65396e1f9f300278ea07f8c38f2e6af68fb9fdba
Anurag Samarth <asamarth017@gmail.com>
2024-03-22 20:31:56 +0530

    FUnction to Increment and Decrement the product quantity

--- Day 34 Ecommerce Website/Ecommerce Website/homeProductCards.js
+++ Day 34 Ecommerce Website/Ecommerce Website/homeProductCards.js
@@ -1,3 +1,5 @@
+import { homeQuantityToggle } from "./homeQuantityToggle";
+
 const productContainer = document.querySelector("#productContainer");
 const productTemplate = document.querySelector("#productTemplate");
 
@@ -11,6 +13,7 @@ export const showProductContainer = (products) => {
         const productClone = document.importNode(productTemplate.content, true);
         // console.log(productClone)
 
+         productClone.querySelector("#cardValue").setAttribute('id',`card${id}`)
         productClone.querySelector(".category").textContent = category;
         productClone.querySelector(".productName").textContent = name;
         productClone.querySelector(".productImage").src = image;
@@ -19,6 +22,10 @@ export const showProductContainer = (products) => {
         productClone.querySelector(".productStock").textContent = stock;
         productClone.querySelector(".productPrice").textContent = `₹${price}`;
         productClone.querySelector(".productActualPrice").textContent = `${price*2}`
+
+        productClone.querySelector(".stockElement").addEventListener('click', (event)=>{
+            homeQuantityToggle(event,id,stock)
+        })
 
         productContainer.append(productClone);
       });
