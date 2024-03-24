37a4dcfb29b8c55a8c1b989b3a8ab3ba2e9ffb0d
Anurag Samarth <asamarth017@gmail.com>
2024-03-25 00:13:59 +0530

    random song play functionality is done

--- Day 35 Music Player/script.js
+++ Day 35 Music Player/script.js
@@ -1,10 +1,18 @@
 let arr = [
     {songName: "Jale 2", url: "./songs/Jale 2.mp3",img:"./images/jale.jpg"},
-    {songName: "Pehle Bhi main", url: "./songs/Pehle Bhe Main.mp3", img: "./images/animal.jpg"},
+    {songName: "Pehle Bhi main", url: "./songs/Pehle Bhi Main.mp3", img: "./images/animal.jpg"},
     {songName: "Ram Siya Ram", url: "./songs/Ram Siya Ram.mp3", img:"./images/ram.jpg"},
     {songName: "Arjan Valley", url: "./songs/Arjan Vailly Ne.mp3", img: "./images/animal.jpg"}
 ]
 
+const allSongs = document.querySelector("#all-songs");
+let poster = document.querySelector("#left")
+
+let audio = new Audio()
+
+let selectedSong = 0
+
+function mainFunction(){
 let clutter = ''
 arr.forEach(function(elem,index){
     clutter += `
@@ -17,4 +25,14 @@ arr.forEach(function(elem,index){
   </div>
     `;
 })
-document.querySelector("#all-songs").innerHTML = clutter;
+allSongs.innerHTML = clutter;
+audio.src = arr[selectedSong].url
+poster.style.backgroundImage = `url(${arr[selectedSong].img})` 
+}
+mainFunction();
+
+allSongs.addEventListener("click", function(dets){
+        selectedSong = dets.target.id;
+        mainFunction()
+        audio.play()
+})
