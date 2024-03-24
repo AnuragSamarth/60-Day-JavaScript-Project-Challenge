22675b3a20e5eaa89b0de440ab4d3d9f69ed2102
Anurag Samarth <asamarth017@gmail.com>
2024-03-25 00:48:03 +0530

    forward and backward functionality is done

--- Day 35 Music Player/script.js
+++ Day 35 Music Player/script.js
@@ -6,7 +6,10 @@ let arr = [
 ]
 
 const allSongs = document.querySelector("#all-songs");
-let poster = document.querySelector("#left")
+let poster = document.querySelector("#left");
+const play = document.querySelector("#play");
+const backward = document.querySelector("#backward");
+const forward = document.querySelector("#forward");
 
 let audio = new Audio()
 
@@ -35,4 +38,39 @@ allSongs.addEventListener("click", function(dets){
         selectedSong = dets.target.id;
         mainFunction()
         audio.play()
+})
+
+let flag = 0;
+play.addEventListener("click", ()=>{
+  if(flag == 0){
+    play.innerHTML = `<i class="ri-pause-line"></i>`
+    mainFunction()
+    audio.play()
+    flag = 1
+  }else{
+    play.innerHTML = `<i class="ri-play-line"></i>`
+    mainFunction()
+    audio.pause()
+    flag = 0;
+  }
+})
+
+forward.addEventListener("click", ()=>{
+  if(selectedSong < arr.length - 1){
+    selectedSong++
+    mainFunction()
+    audio.play()
+  }else{
+    forward.style.opacity = 0.5
+  }
+})
+
+backward.addEventListener("click", ()=>{
+  if(selectedSong >= 0){
+    selectedSong--
+    mainFunction()
+    audio.play()
+  }else{
+    backward.style.opacity = 0.5
+  }
 })
