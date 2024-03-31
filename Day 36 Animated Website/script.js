1628b799fb3cbce2b538859d2de0ff56c387f4d7
Anurag Samarth <asamarth017@gmail.com>
2024-03-31 15:03:08 +0530

    page 7 is done

--- Day 36 Animated Website/script.js
+++ Day 36 Animated Website/script.js
@@ -1,48 +1,60 @@
-function page2Animation(){
-const rightDiv = document.querySelectorAll(".page2-right-div")
+// function page2Animation(){
+// const rightDiv = document.querySelectorAll(".page2-right-div")
 
-rightDiv.forEach((elem)=>{
-     elem.addEventListener("mouseenter", function(e){
-         gsap.to(elem.childNodes[5], {
-            opacity: 1,
-            scale:1
-         })
-     })
-    elem.addEventListener("mouseleave", function(e){
-        gsap.to(elem.childNodes[5], {
-            opacity: 0,
-            scale: 0
-         })
+// rightDiv.forEach((elem)=>{
+//      elem.addEventListener("mouseenter", function(e){
+//          gsap.to(elem.childNodes[5], {
+//             opacity: 1,
+//             scale:1
+//          })
+//      })
+//     elem.addEventListener("mouseleave", function(e){
+//         gsap.to(elem.childNodes[5], {
+//             opacity: 0,
+//             scale: 0
+//          })
+//     })
+//     elem.addEventListener("mousemove", (e)=>{
+//     //    console.log(elem.getBoundingClientRect().y)
+//          gsap.to(elem.childNodes[5],{
+//             x:e.x - elem.getBoundingClientRect().x,
+//             y:e.y - elem.getBoundingClientRect().y - 100
+//          })
+//     })
+// })
+// }
+// page2Animation();
+
+// function page3VideoAnimation(){
+// let page3Center = document.querySelector(".page3-center");
+// let video = document.querySelector("#page3 video")
+
+// page3Center.addEventListener("click", function(){
+//     video.play()
+//     gsap.to(video, {
+//         transform: "scaleX(1) scaleY(1)",
+//         opacity: 1,
+//     })
+// })
+
+// video.addEventListener("click", ()=>{
+//     video.pause()
+//     gsap.to(video, {
+//         transform: "scaleX(0.7) scaleY(0)",
+//         opacity: 0,
+//     })
+// })
+// }
+// page3VideoAnimation()
+
+let sections =  document.querySelectorAll(".section-right");
+sections.forEach(elem => {
+    elem.addEventListener("mouseenter", (e)=>{
+       elem.childNodes[3].style.opacity = 1
+       elem.childNodes[3].play()
     })
-    elem.addEventListener("mousemove", (e)=>{
-    //    console.log(elem.getBoundingClientRect().y)
-         gsap.to(elem.childNodes[5],{
-            x:e.x - elem.getBoundingClientRect().x,
-            y:e.y - elem.getBoundingClientRect().y - 100
-         })
+    elem.addEventListener("mouseleave", (e)=>{
+    elem.childNodes[3].style.opacity = 0;
+    elem.childNodes[3].load();
     })
 })
-}
-page2Animation();
-
-function page3VideoAnimation(){
-let page3Center = document.querySelector(".page3-center");
-let video = document.querySelector("#page3 video")
-
-page3Center.addEventListener("click", function(){
-    video.play()
-    gsap.to(video, {
-        transform: "scaleX(1) scaleY(1)",
-        opacity: 1,
-    })
-})
-
-video.addEventListener("click", ()=>{
-    video.pause()
-    gsap.to(video, {
-        transform: "scaleX(0.7) scaleY(0)",
-        opacity: 0,
-    })
-})
-}
-page3VideoAnimation()
